09ebdef36b2016593c35b846d299d9467f41f77c
a133xz <22479344+a133xz@users.noreply.github.com>
2020-05-26 11:29:13 +0100

    add: 404 🌚

---
+++ src/pages/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="drama">
+    <h2>404: Something is wrong with the world.</h2>
+    <img src="@/assets/images/404.png" alt="a 404 ilustration" width="400" />
+    <h3>Return <g-link to="/">home</g-link>.</h3>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.drama {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-bottom: $spacer-lg;
+  img {
+    padding-top: $spacer-xl;
+    padding-bottom: $spacer-xl;
+  }
+}
+a {
+  border-bottom: 1px solid var(--color-accent);
+}
+</style>
