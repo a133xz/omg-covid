f3f43fa1bf805c8dcc5e0d9d3e4dff2f581b309c
a133xz <22479344+a133xz@users.noreply.github.com>
2020-05-26 11:40:58 +0100

    add: base components 🏠

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <nav>
+      <!-- :to="$tp('/about')" -->
+      <g-link to="/">{{ $t('navigation.home') }}</g-link>
+      <g-link to="/about">
+        {{ $t('navigation.about') }}
+      </g-link>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  position: relative;
+}
+nav {
+  position: absolute;
+  width: 100%;
+  font-weight: bold;
+  top: -2.5rem;
+  text-align: right;
+}
+.active--exact {
+  display: none;
+}
+a:not(.active--exact) {
+  display: inline-block;
+}
+</style>
---
+++ src/layouts/Default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Header />
+    <main>
+      <slot />
+    </main>
+    <Footer />
+    <SpriteIcons />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/TheHeader';
+import Footer from '@/components/TheFooter';
+import SpriteIcons from '@/assets/icons/Sprite';
+
+export default {
+  components: {
+    Header,
+    Footer,
+    SpriteIcons
+  }
+};
+</script>
